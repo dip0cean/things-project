5f87f0d60d4604e7c6b3a0e50256e091cd7f49c6
d0 <dip.0cean@daum.net>
2020-12-22 02:41:54 +0900

    domain.Post -> 생성자 수정

--- src/main/java/com/things/project01/domain/Post.java
+++ src/main/java/com/things/project01/domain/Post.java
@@ -1,10 +1,8 @@
 package com.things.project01.domain;
 
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Table
@@ -12,23 +10,21 @@ import java.time.LocalDateTime;
 @Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Post extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "title",length = 255, nullable =  false)
+    @Column(length = 255, nullable =  false)
     private String title;
 
-    @Column(name = "content" ,length = 255, nullable = false)
+    @Column(length = 255, nullable = false)
     private String content;
 
-    @Column(name = "author",length = 30, nullable = false)
+    @Column(length = 30, nullable = false)
     private String author;
 
     @Column(length = 4, nullable = false)
     private String password;
-
-    public Post() {
-    }
 }
